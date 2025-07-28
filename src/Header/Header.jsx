c6a626c8a3e62998055881253a46930d9a3720a5
Hafsa <hafsatarek4@gmail.com>
2025-07-28 18:57:31 +0200

    I added active navlinks logic

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,20 +1,42 @@
-import { Link, Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
 import "./Header.css";
 
+const navItems = [
+  { label: "Home", to: "hero" },
+  { label: "About", to: "about" },
+  { label: "Skills", to: "skills" },
+  { label: "Projects", to: "projects" },
+  { label: "Get in Touch", to: "contact" },
+];
+
 const Header = () => {
+  const [activeLink, setActiveLink] = useState("hero");
+
+  const handleSetActive = (to) => {
+    setActiveLink(to);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-pink">
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">
+        <ScrollLink
+          className="navbar-brand fw-bold"
+          to="hero"
+          smooth={true}
+          duration={200}
+          offset={-70}
+          onClick={() => handleSetActive("hero")}
+        >
           Hafsa Hikal
-        </Link>
+        </ScrollLink>
+
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
         >
-          {/* toggler buton needs some fix */}
           <span className="navbar-toggler-icon"></span>
         </button>
 
@@ -23,61 +45,22 @@ const Header = () => {
           id="navbarNav"
         >
           <ul className="navbar-nav gap-3">
-            <li className="nav-item">
-              <ScrollLink
-                className="nav-link"
-                to="hero"
-                smooth={true}
-                duration={200}
-                offset={-70}
-              >
-                Home
-              </ScrollLink>
-            </li>
-            <li className="nav-item">
-              <ScrollLink
-                className="nav-link"
-                to="about"
-                smooth={true}
-                duration={200}
-                offset={-70}
-              >
-                About
-              </ScrollLink>
-            </li>
-            <li className="nav-item">
-              <ScrollLink
-                className="nav-link"
-                to="skills"
-                smooth={true}
-                duration={200}
-                offset={-70}
-              >
-                Skills
-              </ScrollLink>
-            </li>
-            <li className="nav-item">
-              <ScrollLink
-                className="nav-link"
-                to="projects"
-                smooth={true}
-                duration={200}
-                offset={-70}
-              >
-                Projects
-              </ScrollLink>
-            </li>
-            <li className="nav-item">
-              <ScrollLink
-                className="nav-link"
-                to="contact"
-                smooth={true}
-                duration={200}
-                offset={-70}
-              >
-                Get in Touch
-              </ScrollLink>
-            </li>
+            {navItems.map((item) => (
+              <li className="nav-item" key={item.to}>
+                <ScrollLink
+                  className={`nav-link ${
+                    activeLink === item.to ? "active" : ""
+                  }`}
+                  to={item.to}
+                  smooth={true}
+                  duration={200}
+                  offset={-70}
+                  onClick={() => handleSetActive(item.to)}
+                >
+                  {item.label}
+                </ScrollLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
