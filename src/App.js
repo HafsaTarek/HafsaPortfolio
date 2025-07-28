11c813feef28fc5a37508b204588e14ab93d2a48
Hafsa <hafsatarek4@gmail.com>
2025-07-28 15:00:31 +0200

    I deleted the dark scheme feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import "./App.css";
 import Header from "./Header/Header";
@@ -10,19 +10,6 @@ import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
 
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    // Automatically set theme based on system preference
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    setTheme(prefersDark ? "dark" : "light");
-  }, []);
-
-  useEffect(() => {
-    document.body.className = theme;
-  }, [theme]);
   return (
     <div className="App">
       <Header />
